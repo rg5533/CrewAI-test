2ac832081f3ff2adcc9022f5eb3a02f53b34d111
Rohit Gupta <rohitgupta5533@gmail.com>
2024-11-14 23:25:00 +1100

    Initial commit

---
+++ src/edu/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from edu.crew import Edu
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# Must precede any llm module imports
+
+from langtrace_python_sdk import langtrace
+
+langtrace.init(api_key = '9581d0d32b648dc5fc508b7a3ca51adbf13ef99749c2ef3785918022e0694c22')
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs'
+    }
+    Edu().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        Edu().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        Edu().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        Edu().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
